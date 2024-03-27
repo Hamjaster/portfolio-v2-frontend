d9428ccc45fce26d3ff04a8a0971712fd248aff2
Hamza <hamzashah.dev@gmail.com>
2024-03-27 18:11:09 +0500

    optimizations + video on hover + image change

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,44 +1,83 @@
-import React, { useContext } from 'react'
-import { MyContext } from '../context/ContextProvider'
-import { FaGithub } from 'react-icons/fa'
+import React, { useContext, useRef } from "react";
+import { MyContext } from "../context/ContextProvider";
+import { FaGithub } from "react-icons/fa";
 
 export default function Project({ project }) {
-    const { theme } = useContext(MyContext)
-    return (
-        <>
+  const { theme } = useContext(MyContext);
+  const videoRef = useRef(null);
 
-            <div className={`relative border-2 mb-5 border-${theme}-600 md:h-72 flex md:flex-row flex-col`}>
+  const handleMouseEnter = () => {
+    if (videoRef.current) {
+      videoRef.current.style.display = "block";
+      videoRef.current.play();
+    }
+  };
 
-                {/* Project Image BG */}
-                <div style={{
-                    background: `url(${project.img}) center/cover`
-                }} className="md:w-2/5 h-56 md:h-full">
-                </div>
+  const handleMouseLeave = () => {
+    if (videoRef.current) {
+      videoRef.current.style.display = "none";
+      videoRef.current.pause();
+    }
+  };
 
-                {/* Info */}
-                <div className={`info font-[Roboto] md:w-3/5 mx-auto relative z-40 bg-${theme}-600 text-white font-semibold md:text-4xl text-2xl items-start justify-between flex flex-col px-4 md:py-7 py-4 space-y-3`}>
+  const getEmbeddedUrl = (googleDriveUrl) => {
+    // Extract the VIDEO_ID from the Google Drive URL
+    const VIDEO_ID = googleDriveUrl.match(/\/file\/d\/(.+?)\//)[1];
+    // Construct the embedded URL
+    return `https://drive.google.com/uc?export=download&id=${VIDEO_ID}`;
+  };
 
-                    <div className='text-start md:space-y-3 space-y-1'>
-                        <span className=''>{project.title}</span>
-                        <div className='font-light text-sm md:text-lg '>{project.desc}</div>
-                    </div>
+  return (
+    <>
+      <div
+        className={`relative border-2 mb-5 border-${theme}-600 md:h-72 flex md:flex-row flex-col`}
+      >
+        {/* Project Image BG */}
+        <div
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+          className="md:w-2/5 h-56 md:h-full relative"
+        >
+          <video
+            ref={videoRef}
+            src={project.video}
+            className="absolute top-0 left-0 w-full h-full object-cover"
+            style={{ display: "none" }}
+          ></video>
+          <div
+            style={{
+              background: `url(${project.img}) center/cover`,
+            }}
+            className="w-full h-full"
+          ></div>
+        </div>
 
-                    <div className="text-start w-full flex flex-row  justify-between items-center ">
-                        <a target='_' className={`text-lg text-center md:w-36 block w-full border hover:bg-slate-200 border-slate-50 text-${theme}-700 md:rounded-md rounded-sm bg-white md:px-4 px-2 md:py-2 py-1`} href={project.link}>View</a>
+        {/* Info */}
+        <div
+          className={`info font-[Roboto] md:w-3/5 mx-auto relative z-40 bg-${theme}-600 text-white font-semibold md:text-4xl text-2xl items-start justify-between flex flex-col px-4 md:py-7 py-4 space-y-3`}
+        >
+          <div className="text-start md:space-y-3 space-y-1">
+            <span className="">{project.title}</span>
+            <div className="font-light text-sm md:text-lg ">{project.desc}</div>
+          </div>
 
-                        <a target='_' href={project.github}>
-                            <div className="git cursor-pointer text-gray-200 hover:text-white">
-                                <FaGithub />
-                            </div>
-                        </a>
+          <div className="text-start w-full flex flex-row  justify-between items-center ">
+            <a
+              target="_"
+              className={`text-lg text-center md:w-36 block w-full border hover:bg-slate-200 border-slate-50 text-${theme}-700 md:rounded-md rounded-sm bg-white md:px-4 px-2 md:py-2 py-1`}
+              href={project.link}
+            >
+              View
+            </a>
 
-
-                    </div>
-
-                </div>
-
-            </div>
-
-        </>
-    )
+            <a target="_" href={project.github}>
+              <div className="git cursor-pointer opacity-70 hover:opacity-100">
+                <FaGithub />
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
